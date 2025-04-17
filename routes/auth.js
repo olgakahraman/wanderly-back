3280622e3fd5d699eaa0edaaf6c36930529f841f
olgakahraman <ol.khrmn@gmail.com>
2025-04-17 12:05:12 -0400

    validation is added

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router = express.Router();
-
+const { check, validationResult } = require('express-validator');
 const { register, login } = require('../controllers/auth');
 
-router.post('/register', register);
-router.post('/login', login);
+router.post(
+  '/register',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password must be 6+ characters').isLength({ min: 6 }),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next;
+  },
+  register
+);
+router.post(
+  '/login',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').notEmpty(),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+  login
+);
 
 module.exports = router;
