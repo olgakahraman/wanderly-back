e7d4900aa55bc7f84acc498e674f51d180f2ea52
olgakahraman <117988074+olgakahraman@users.noreply.github.com>
2025-06-10 21:10:27 -0400

    Revert "add map page where you can mark visited countries"
    This reverts commit 94e5ef037984d24e794ca613211615970caa41e1.

--- app.js
+++ app.js
@@ -2,29 +2,18 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 const cors = require('cors');
+const xss = require('xss-clean');
 const posts = require('./routes/posts');
 const authRoute = require('./routes/auth');
 const authMiddleware = require('./middleware/auth.js');
 const notFound = require('./middleware/notFound');
 const errorHandlerMiddleware = require('./middleware/errorHandler');
-const path = require('path');
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 const connectDB = require('./db/connect');
 require('dotenv').config();
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
-
-const session = require('express-session');
-app.use(
-  session({
-    secret: 'wanderly_temp_secret',
-    resave: false,
-    saveUninitialized: true,
-  })
-);
 
 const allowedOrigins = [
   'http://localhost:5173',
@@ -39,9 +28,9 @@ app.use(
   })
 );
 app.use(helmet());
+app.use(xss());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path}`);
   next();
@@ -51,31 +40,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/hello', (req, res) => {
   res.send('Wanderly app server side is here');
-});
-
-app.get('/travel-map', (req, res) => {
-  const visitedCountries = req.session.visitedCountries || [];
-  res.render('travel-map', {
-    user: req.session.user || 'Guest',
-    visitedCountries,
-    countries: ['Italy', 'Japan', 'Mexico', 'Norway', 'Thailand'], 
-  });
-});
-
-app.post('/travel-map', (req, res) => {
-  if (!req.session.visitedCountries) {
-    req.session.visitedCountries = [];
-  }
-
-  if (
-    req.body.country &&
-    !req.session.visitedCountries.includes(req.body.country)
-  ) {
-    req.session.visitedCountries.push(req.body.country);
-    req.session.user = 'OLGA';
-  }
-
-  res.redirect('/travel-map');
 });
 
 app.use('/api/v1/auth', authRoute);
